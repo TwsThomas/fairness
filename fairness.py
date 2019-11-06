8204a863b4db88b3cd8e71f62313aa73ac6bce7a
twsthomas <twsthomas@gmail.com>
2019-11-06 17:10:00 +0100

    wip tuto

--- fairness.py
+++ fairness.py
@@ -17,109 +17,175 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-def fairness(y_pred, y_test, G_test, verbose=False):
-    
-    G_test = np.array(G_test, dtype=bool)
-    y_pred = np.array(y_pred, dtype=bool)
-    y_test = np.array(y_test, dtype=bool)
-    if verbose:
-        for name, mask in zip(['G', '~G', 'all'], [G_test, ~G_test, [True]*len(y_test)]):
-            print('---------', name, '------------')
-            # print('Accuraccy:', accuracy_score(y_test[mask], y_pred[mask]))
-            print('Error (1-Acc.):', round(1-accuracy_score(y_test[mask], y_pred[mask]),4))
-            print(confusion_matrix(y_test[mask], y_pred[mask]) / sum(mask))
-            print('% of positive preds =', y_pred[mask].sum() / len(y_pred[mask]))
+import matplotlib.pyplot as plt
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
 
-        print('.\n')
-    
-    Zg_test = sum(G_test) / len(y_test)
-    
-    P = sum(y_pred) # Positive prediction
-    P_G = sum(y_pred * G_test) # Positive prediction on G
-    P_test = sum(y_test) # Positive label (in y_test)
-    P_test_G = sum(y_test * G_test) # Positive label on G (in y_test)
-    TP = sum(y_pred * y_test) # True Positif 
-    TP_G = sum(y_pred * G_test * y_test) # True Positif on G
-    
-    fair_dp = P_G / Zg_test - P
-    fair_dp /= len(y_pred)
-    if verbose:
-        print('fair Demo. Parity =\t', round(fair_dp,5))
-    
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    # classes = classes[unique_labels(y_true, y_pred)]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        # print("Normalized confusion matrix")
+    else:
+        pass
+        # print('Confusion matrix, without normalization')
 
-    fair_eop = TP_G / P_test_G - TP / P_test
-    fair_eop /= len(y_pred)
-    if verbose:
-        print('fair Equal Opp.   =\t', round(fair_eop,5))
-    
-    return fair_dp, fair_eop
+    # print(cm)
 
-def show_score(y_pred, y_test, G, verbose=False):
-    #  return ['err_G', 'err', 'fair_dp', 'fair_eop'], 
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+                #    vmin=0, vmax=1)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
 
-    G_test = np.array(G, dtype=bool)
-    
-    if verbose:
-        print('Error (1-Accuraccy):')
-        print('on G \t on X')
-    score = []
-    for _, mask in zip(['G', 'all'], [G_test, [True]*len(y_test)]):
-        score.append(1-accuracy_score(y_test[mask], y_pred[mask]))
-        if verbose:
-            print(round(score[-1],4), end='\t')
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
 
-    fair_dp, fair_eop = fairness(y_pred, y_test, G, verbose=False)
-    if verbose:
-        print()
-        print('fair_dp \t fair_eop')
-        print(round(fair_dp,4),'\t', round(fair_eop,4))
-    
-    #  return ['err_G', 'err', 'fair_dp', 'fair_eop'], 
-    return score + [fair_dp, fair_eop]
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
 
 
-def get_w(lambda_, Gk, y_train):
-    y_train = np.array(y_train, dtype=bool)
-    re_weight = np.array([lambda_[k] * Gk[k] for k in range(len(Gk))])
-    w_tilde = np.exp(np.sum(re_weight, axis = 0))  # n-vectors
-    assert w_tilde.shape[0] == (len(y_train))
-    w = np.array(w_tilde / (1 + w_tilde) * y_train +\
-        1 / (1 + w_tilde) * (1 - y_train))
+def fairness_violation(y_pred, y_test, protected_group,
+                       notion='demographic_parity'):
+    # Compute the fairness violation w.r.t. the protected group
+
+    if notion=='demographic_parity':
+        # demographic_parity compare the positive prediction rate 
+        
+        # Cardinality on each group
+        nb_pg = sum(protected_group)  
+        nb_X = len(y_pred)
+        # Positive prediction
+        P_X = sum(y_pred)   
+        P_pg = sum(y_pred * protected_group)
+        
+        demographic_parity_violation = P_pg / nb_pg - P_X / nb_X
+        return demographic_parity_violation
+
+    elif notion=='equal_opportunity':
+        # equal_opportunity compare true positive rates
+
+        # Positive true label
+        P_true_X = sum(y_test) 
+        P_true_pg = sum(y_test * protected_group) 
+        # True Positif
+        TP_X = sum(y_pred * y_test) 
+        TP_pg = sum(y_pred * protected_group * y_test)    
+
+        equal_opportunity_violation = TP_pg / P_true_pg - TP_X / P_true_X
+        return equal_opportunity_violation
+
+
+def reweigth_samples(lambda_, protected_group, y_bias):
+    # Compute the reweighting w.r.t the protected group
+    
+    w_tilde = np.exp(lambda_ * protected_group)
+    w = np.array(w_tilde / (1 + w_tilde) * y_bias +\
+                 1 / (1 + w_tilde) * (1 - y_bias))
     return w
 
 
-def fairness_error(lr, X, y_true, Gk, notion = 'dp'):
+
+
+# def show_score(y_pred, y_test, G, verbose=False):
+#     #  return ['err_G', 'err', 'fair_dp', 'fair_eop'], 
+
+#     G_test = np.array(G, dtype=bool)
     
-    y_pred = np.array(lr.predict(X), dtype=bool)
-    y_true = np.array(y_true, dtype=bool)
+#     if verbose:
+#         print('Error (1-Accuraccy):')
+#         print('on G \t on X')
+#     score = []
+#     for _, mask in zip(['G', 'all'], [G_test, [True]*len(y_test)]):
+#         score.append(1-accuracy_score(y_test[mask], y_pred[mask]))
+#         if verbose:
+#             print(round(score[-1],4), end='\t')
+
+#     fair_dp, fair_eop = fairness(y_pred, y_test, G, verbose=False)
+#     if verbose:
+#         print()
+#         print('fair_dp \t fair_eop')
+#         print(round(fair_dp,4),'\t', round(fair_eop,4))
     
-    l_dp = []
-    l_eop = []
+#     #  return ['err_G', 'err', 'fair_dp', 'fair_eop'], 
+#     return score + [fair_dp, fair_eop]
+
+
+
+
+# def reweigth_samples(lambda_, protected_groups, y_bias):
+#     # Compute the reweighting w.r.t the protected group
+
+#     re_weight = np.array([lambda_[k] * Gk[k] for k in range(len(Gk))])
+#     w_tilde = np.exp(np.sum(re_weight, axis = 0))  # n-vectors
+#     assert w_tilde.shape[0] == (len(y_train))
+#     w = np.array(w_tilde / (1 + w_tilde) * y_train +\
+#         1 / (1 + w_tilde) * (1 - y_train))
+#     return w
+
+
+# def fairness_error(lr, X, y_true, Gk, notion = 'dp'):
+    
+#     y_pred = np.array(lr.predict(X), dtype=bool)
+#     y_true = np.array(y_true, dtype=bool)
+    
+#     l_dp = []
+#     l_eop = []
         
-    for G in Gk:
-        G = np.array(G, dtype=bool)
+#     for G in Gk:
+#         G = np.array(G, dtype=bool)
     
-        Zg_test = sum(G) / len(y_true)
+#         Zg_test = sum(G) / len(y_true)
 
-        P = sum(y_pred) # Positive prediction
-        P_G = sum(y_pred * G) # Positive prediction on G
-        P_test = sum(y_true) # Positive label (in y_true)
-        P_test_G = sum(y_true * G) # Positive label on G (in y_true)
-        TP = sum(y_pred * y_true) # True Positif 
-        TP_G = sum(y_pred * y_true * G) # True Positif on G
+#         P = sum(y_pred) # Positive prediction
+#         P_G = sum(y_pred * G) # Positive prediction on G
+#         P_test = sum(y_true) # Positive label (in y_true)
+#         P_test_G = sum(y_true * G) # Positive label on G (in y_true)
+#         TP = sum(y_pred * y_true) # True Positif 
+#         TP_G = sum(y_pred * y_true * G) # True Positif on G
 
-        fair_dp = abs(P_G / Zg_test - P)
-        fair_dp /= len(y_pred)
+#         fair_dp = abs(P_G / Zg_test - P)
+#         fair_dp /= len(y_pred)
 
-        fair_eop = abs(TP_G / P_test_G - TP / P_test)
-        fair_eop /= len(y_pred)
+#         # print('eop', TP_G, P_test_G, TP, P_test, abs(TP_G / P_test_G - TP / P_test))
+#         fair_eop = abs(TP_G / P_test_G - TP / P_test) # violation
+#         fair_eop /= len(y_pred)
 
-        l_dp.append(fair_dp)
-        l_eop.append(fair_eop)
+#         l_dp.append(fair_dp)
+#         l_eop.append(fair_eop)
 
-    if notion == 'dp':
-        return np.array(l_dp)
-    elif notion == 'eop':
-        return np.array(l_eop)
-    else:
-        return None
+#     if notion == 'dp':
+#         return np.array(l_dp)
+#     elif notion == 'eop':
+#         return np.array(l_eop)
+#     else:
+#         return None
--- get_data.py
+++ get_data.py
@@ -2,86 +2,125 @@ import numpy as np
 import copy
 import pandas as pd
 
+
+from sklearn.compose import ColumnTransformer
+
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+
 from sklearn.model_selection import train_test_split
 
+from joblib import Memory, Parallel, delayed
+location = './cachedir'
+memory = Memory(location, verbose=0)
 
-def get_bank(return_df=False):
-    df = pd.read_csv('data/bank/bank-full.csv', sep=";")
-    y = df['y'] == 'yes'
-    X = pd.get_dummies(df.drop('y', axis=1))
 
-    if return_df:
-        return X, y
+@memory.cache
+def get_adult(random_state=42):
+
+    df = pd.read_csv(
+    "https://www.openml.org/data/get_csv/1595261/adult-census.csv")
+    # Or use the local copy:
+    # df = pd.read_csv('../datasets/adult-census.csv')
+    print(df.shape)
+    target_name = "class"
+    target = df[target_name].to_numpy()
+    target = target == ' >50K'
+
+    protected_groups = [df['sex']==' Female', df['sex'] ==' Male',
+                        df['race'] == ' Black', df['race'] == ' White']
+
+    data = df.drop(columns=[target_name, "fnlwgt"])
+
+    gg = np.asarray(protected_groups).T
+    print(gg.shape)
+    data_train, data_test, target_train, target_test,\
+         protected_train_T, protected_test_T = train_test_split(
+        data, target, gg, random_state=random_state)
+
+    # preprocessing steps
+    binary_encoding_columns = ['sex']
+    one_hot_encoding_columns = [
+        'workclass', 'education', 'marital-status', 'occupation',
+        'relationship', 'race', 'native-country']
+    scaling_columns = [
+        'age', 'education-num', 'hours-per-week', 'capital-gain',
+        'capital-loss']
+
+    preprocessor = ColumnTransformer([
+        ('binary-encoder', OrdinalEncoder(), binary_encoding_columns),
+        ('one-hot-encoder', OneHotEncoder(handle_unknown='ignore'),
+        one_hot_encoding_columns),
+        ('standard-scaler', StandardScaler(), scaling_columns)])
+
+    data_train = preprocessor.fit_transform(data_train)
+    data_test = preprocessor.transform(data_test)
+
+    protected_train, protected_test = protected_train_T.T, protected_test_T.T
+    if len(protected_train) == 4:
+        protected_train = protected_train[0]
+        protected_test = protected_test[0]
+
+    return data_train, data_test, target_train, target_test,\
+           protected_train, protected_test
+
+
+
+
+# def get_gk_adult(X, y=None, ohe=False):
+#     # return the 4 protected group (sex & white/black)
     
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.30, random_state=42)
-    # print('age quantile =', np.quantile(X['age'], [i/6 for i in range(1,6)]))
-    return X_train, y_train, X_test, y_test
-
-
-def get_gk_bank(X, y=None):
-    # return the 5 protected group (by age)
+#     if not(ohe):
+#         Gk = [X['sex']=='Female', X['sex'] =='Male', X['race'] == 'Black', X['race'] == 'White']
+#     else:
+#         Gk = [X['sex_Female'], X['sex_Male'], X['race_Black'], X['race_White']]
     
-    age_q = list(np.quantile(X['age'], [i/6 for i in range(1,6)]))
-    print('age quantile =', age_q)
+#     Gk = [np.array(g, dtype=bool) for g in Gk]
+#     return Gk
+
+
+# def get_adult(return_df=False):
     
-    age_bins = [0] + age_q + [max(X['age'])+1]
-    Gk = []
-    for min_, max_ in zip(age_bins[:-1], age_bins[1:]):
-        # print(min_, max_)
-        Gk.append(np.array((X['age'] >= min_) & (X['age'] < max_)).copy())
-        
-    assert X.shape[0] == sum([x.sum() for x in Gk])
-    Gk = [np.array(g, dtype=bool) for g in Gk]
-    return Gk
+#     # read data
 
+#     adult_columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']
+#     df_train = pd.read_csv('data/adult.data', header=None, sep=', ', engine='python')
+#     df_train.columns = adult_columns + ['y']
+#     df_test = pd.read_csv('data/adult.test.txt', header=None, sep=', ', skiprows=1, engine='python')
+#     df_test.columns = adult_columns + ['y']
 
-def get_gk_adult(X, y=None):
-    # return the 4 protected group (sex & white/black)
-    Gk = [X['sex_Female'], X['sex_Male'], X['race_Black'], X['race_White']]
-    Gk = [np.array(g, dtype=bool) for g in Gk]
-    return Gk
+#     X_train, y_train, X_test, y_test = df_train.drop('y', axis=1), df_train['y'], df_test.drop('y', axis=1), df_test['y']
+#     y_train = y_train == '>50K'
+#     y_test = y_test == '>50K.'
+#     print('adult loaded')
+#     print('X,y train/test shape', X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 
-def get_adult(return_df=False):
+#     if return_df:
+#         return X_train, y_train, X_test, y_test
     
-    # read data
+#     ## OHE
+#     cat_columns = ['workclass', 'education', 'education-num', 'marital-status', 'occupation',
+#                    'relationship', 'race', 'sex', 'native-country']
+#     X_train_ohe = pd.get_dummies(X_train, columns=cat_columns)
+#     # HACK for same OHE
+#     X_test.at[X_test.index[-1], 'native-country'] = 'Holand-Netherlands'
+#     X_test_ohe = pd.get_dummies(X_test, columns=cat_columns)
+#     print('X_train_ohe X_test_ohe shape:')
+#     print(X_train_ohe.shape, X_test_ohe.shape)
 
-    adult_columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country']
-    df_train = pd.read_csv('data/adult.data', header=-1, sep=', ', engine='python')
-    df_train.columns = adult_columns + ['y']
-    df_test = pd.read_csv('data/adult.test.txt', header=-1, sep=', ', skiprows=1, engine='python')
-    df_test.columns = adult_columns + ['y']
-
-    X_train, y_train, X_test, y_test = df_train.drop('y', axis=1), df_train['y'], df_test.drop('y', axis=1), df_test['y']
-    y_train = y_train == '>50K'
-    y_test = y_test == '>50K.'
-    # print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
-
-    if return_df:
-        return X_train, y_train, X_test, y_test
+#     X_all = pd.get_dummies(pd.concat((X_train, X_test)), columns=cat_columns)
+#     y_all = pd.concat((y_train, y_test))
+#     shape_all = X_all.shape
+#     # print(shape_all)
+#     assert X_train_ohe.shape[1] == X_test_ohe.shape[1]
+#     assert X_train_ohe.shape[1] == shape_all[1]
     
-    ## OHE
-    cat_columns = ['workclass', 'education', 'education-num', 'marital-status', 'occupation',
-                   'relationship', 'race', 'sex', 'native-country']
-    X_train_ohe = pd.get_dummies(X_train, columns=cat_columns)
-    # HACK for same OHE
-    X_test['native-country'].iloc[-1] = 'Holand-Netherlands'
-    X_test_ohe = pd.get_dummies(X_test, columns=cat_columns)
-    print(X_train_ohe.shape, X_test_ohe.shape)
-
-    X_all = pd.get_dummies(pd.concat((X_train, X_test)), columns=cat_columns)
-    y_all = pd.concat((y_train, y_test))
-    shape_all = X_all.shape
-    # print(shape_all)
-    assert X_train_ohe.shape[1] == X_test_ohe.shape[1]
-    assert X_train_ohe.shape[1] == shape_all[1]
+#     assert X_train_ohe.shape[0] == y_train.shape[0]
+#     assert X_test_ohe.shape[0] == y_test.shape[0]
     
-    assert X_train_ohe.shape[0] == y_train.shape[0]
-    assert X_test_ohe.shape[0] == y_test.shape[0]
-    
-    assert X_train_ohe.shape[0] + X_test_ohe.shape[0] == shape_all[0]
-    assert set(X_train_ohe.columns).difference(set(X_test_ohe.columns)) == set()
-    assert set(X_test_ohe.columns).difference(set(X_train_ohe.columns)) == set()
+#     assert X_train_ohe.shape[0] + X_test_ohe.shape[0] == shape_all[0]
+#     assert set(X_train_ohe.columns).difference(set(X_test_ohe.columns)) == set()
+#     assert set(X_test_ohe.columns).difference(set(X_train_ohe.columns)) == set()
 
 
     # #sklearn version 
@@ -97,3 +136,35 @@ def get_adult(return_df=False):
 #         X_all, y_all, test_size=0.30, random_state=42)
 
     return X_train_ohe, y_train, X_test_ohe, y_test
+
+
+# def get_bank(return_df=False, path='data/bank/bank-full.csv'):
+#     print('load ', path)
+#     df = pd.read_csv(path, sep=";")
+#     y = df['y'] == 'yes'
+#     X = pd.get_dummies(df.drop('y', axis=1))
+
+#     if return_df:
+#         return X, y
+    
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         X, y, test_size=0.30, random_state=42)
+#     # print('age quantile =', np.quantile(X['age'], [i/6 for i in range(1,6)]))
+#     return X_train, y_train, X_test, y_test
+
+
+# def get_gk_bank(X, y=None):
+#     # return the 5 protected group (by age)
+    
+#     age_q = list(np.quantile(X['age'], [i/6 for i in range(1,6)]))
+#     print('age quantile =', age_q)
+    
+#     age_bins = [0] + age_q + [max(X['age'])+1]
+#     Gk = []
+#     for min_, max_ in zip(age_bins[:-1], age_bins[1:]):
+#         # print(min_, max_)
+#         Gk.append(np.array((X['age'] >= min_) & (X['age'] < max_)).copy())
+        
+#     assert X.shape[0] == sum([x.sum() for x in Gk])
+#     Gk = [np.array(g, dtype=bool) for g in Gk]
+#     return Gk
